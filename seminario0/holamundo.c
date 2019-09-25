f751272247fb64a61c8f214956620ed2896ae213
Javier1zq <you@example.com>
2019-09-25 15:45:17 +0200

    Añadido libreria stdlib a holamundo.c

--- seminario0/holamundo.c
+++ seminario0/holamundo.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	int a;
 	printf("Introduce un numero del 1-10: ");
 	scanf("%i",&a);
-	printf("Has pulsado el - %i",a );
+	printf("Has introducido el - %i\n", a);
 	return 0;
 }
