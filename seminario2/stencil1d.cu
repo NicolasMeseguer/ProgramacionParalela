d150658456408f8d7c9515751c6df19f7122aea0
NicolasMeseguer <nicolasmeseguer@icloud.com>
2019-11-27 12:00:11 +0100

    Añadido Stencil/Jacobi 1D con error logico...

--- seminario2/stencil1d.cu
+++ seminario2/stencil1d.cu
@@ -4,46 +4,54 @@
 #include <string.h>
 #include <math.h>
 
-#define TAM 10
+#define TAM 8
+#define NITERACIONES 40
 #define TAMBLOCK 2
 
-__global__ void sumaVectores(float *b, float *a){ //Kernel, salto a la GPU. Esta funcion es ejecutada por todos los hilos al mismo tiempo.
-  	int i = blockIdx.x*blockDim.x+threadIdx.x; //Obtengo el indice para cada iteracion de la funcion sobre cada hilo
-    if(i<TAM && i!=0 && i!=TAM-1)
-		  b[i]=(a[i-1]*a[i]*a[i+1])/3;
+__global__ void stencil(float *a, float *b){ //Kernel, salto a la GPU. Esta funcion es ejecutada por todos los hilos al mismo tiempo.
+  int i = blockIdx.x*blockDim.x+threadIdx.x+1;
+  b[i]=(a[i-1]+a[i]+a[i+1])/3;
 }
 
 int main() {
 	int memsize = sizeof(float)*TAM;
-	
   float *h_a,*h_b; //Arrays en el host (CPU & RAM)
 	h_a=(float *)malloc(memsize);
 	h_b=(float *)malloc(memsize);
+
+  for(int i=0;i<TAM;++i){
+    h_a[i]=h_b[i]=(float)(rand()%20);
+  }
 
   float *d_a,*d_b; //Arrays en la GPU
   cudaMalloc(&d_a, memsize);
   cudaMalloc(&d_b, memsize);
 
-	for(int i=0; i<TAM; ++i)
-		h_a[i]=(float)(rand()%50);
-
-  //Envio el contenido de los arrays(h_a, h_b) CONTENIDO ! Al espacio de memoria ya reservado en la GPU(d_a, d_b). CPU -> GPU | Host -> Device
   cudaMemcpy(d_a, h_a, memsize, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_a, memsize, cudaMemcpyHostToDevice);
-    
-  int block = ceilf((float)TAM/TAMBLOCK);
-  int thread = TAMBLOCK;
-  printf("El numero de bloques es %d, y el numero de hilos es %d\n", block, thread);
-  sumaVectores <<<block,thread>>> (d_b, d_a);//El multiplicar ambos numeros tiene que darme N
+  cudaMemcpy(d_b, h_b, memsize, cudaMemcpyHostToDevice);
+ 
+  dim3 block((TAM-2)/TAMBLOCK);
+  dim3 thread(TAMBLOCK); 
+  printf("El numero de bloques es %d, y el numero de hilos es %d\n", block.x, thread.x);
+  for(int j=0;j<NITERACIONES;++j){
+    stencil <<<block,thread>>> (d_a, d_b);
+    cudaThreadSynchronize();
+    float *aux=d_b;
+    d_b=d_a;
+    d_a=aux;
+  }
 
-  //Envio el contenido del array(d_c) CONTENIDO ! Al espacio de memoria ya reservado en la CPU(h_c). GPU -> CPU | Device -> Host
+  cudaMemcpy(h_a, d_a, memsize, cudaMemcpyDeviceToHost);
   cudaMemcpy(h_b, d_b, memsize, cudaMemcpyDeviceToHost);
 
-	printf("Resultado del tercer vector, c: \n");
-	for(int i=0; i<TAM; ++i)
-		printf("%f, ", h_b[i]);
-
-	printf("\n");
+  printf("Vector A:\n");
+  for(int i=0;i<TAM;++i)
+    printf("%f, ", *(h_a+i));
+  
+  printf("\n\nVector B:\n");
+  for(int i=0;i<TAM;++i)
+    printf("%f, ", *(h_b+i));
+  printf("\n");
 
   free(h_a);
   free(h_b);
